7d29b8bd272f4cd76f99b083f6ce5b0ba6ae512a
KTG5672 <zzang5992@gmail.com>
2025-05-24 17:33:54 +0900

    chore: 코드 리뷰 피드백 반영 - 각 클래스 및 메서드 주석 추가

--- src/main/java/io/hhplus/tdd/point/PointChargePolicy.java
+++ src/main/java/io/hhplus/tdd/point/PointChargePolicy.java
@@ -2,12 +2,23 @@ package io.hhplus.tdd.point;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * 포인트 충전 정책 검증 클래스
+ * - validate 메서드를 이용하여 충전 요청이 유효한지 검증한다.
+ */
 @Service
 public class PointChargePolicy {
 
     private static final long MAX_POINT = 1_000_000L;
     private static final long MIN_CHARGE_POINT = 1L;
 
+    /**
+     * 보유 포인트와 충전 포인트를 받아 충전 정책 검증한다.
+     * - 충전 포인트가 1미만이면 IllegalArgumentException 발생 시킨다.
+     * - 보유 포인트와 충전 포인트 합산이 최대 한도를 초과하면 IllegalStateException 발생 시킨다.
+     * @param existingPoint 보유 포인트
+     * @param chargePoint 충전 포인트
+     */
     public void validate(long existingPoint, long chargePoint) {
         if (chargePoint < MIN_CHARGE_POINT) {
             throw new IllegalArgumentException("충전 금액은 0보다 커야 합니다.");
--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -5,6 +5,11 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.ReentrantLock;
 import org.springframework.stereotype.Service;
 
+/**
+ * 사용자별 포인트 충전, 사용, 조회 기능을 제공하는 서비스 클래스
+ *  - 충전, 사용 기능은 순차적으로 처리되어야 하므로 ReentrantLock 을 사용하여 동시성 제어
+ *  - 사용자 ID 를 사용하여 개별 락으로 충전, 사용 요청의 동시성 제어
+ */
 @Service
 public class PointService {
 
@@ -23,6 +28,14 @@ public class PointService {
         this.pointHistoryService = pointHistoryService;
     }
 
+    /**
+     * 포인트 충전 기능
+     * - ReentrantLock 을 이용하여 동시성 제어
+     * - 충전 정책을 검증 후 포인트를 합산하여 저장
+     * @param id 사용자 ID
+     * @param chargePoint 충전 포인트
+     * @return UserPoint 충전 후 포인트 상태
+     */
     public UserPoint charge(Long id, Long chargePoint) {
         ReentrantLock lock = getUserPointLock(id);
         lock.lock();
@@ -41,6 +54,14 @@ public class PointService {
         }
     }
 
+    /**
+     * 포인트 사용 기능
+     * - ReentrantLock 을 이용하여 동시성 제어
+     * - 사용 정책을 검증 후 포인트를 차감하여 저장
+     * @param id 사용자 ID
+     * @param usePoint 사용 포인트
+     * @return UserPoint 사용 후 포인트 상태
+     */
     public UserPoint use(Long id, Long usePoint) {
         ReentrantLock lock = getUserPointLock(id);
         lock.lock();
@@ -60,10 +81,21 @@ public class PointService {
         }
     }
 
+    /**
+     * 사용자 ID 별 포인트 정보 조회 기능
+     * @param id 사용자 ID
+     * @return UserPoint 해당 사용자 포인트 상태
+     */
     public UserPoint getUserPoint(Long id) {
         return userPointTable.selectById(id);
     }
 
+    /**
+     * 사용자 ID 를 기반으로 받아 락을 반환
+     * - 락 정보가 없으면 생성하여 반환
+     * @param id 사용자 ID
+     * @return ReentrantLock 사용자 별 락
+     */
     private ReentrantLock getUserPointLock(Long id) {
         return userPointLocks.computeIfAbsent(id, k -> new ReentrantLock());
     }
