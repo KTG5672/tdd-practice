7018141daec049e393f606b64ef8db99685a7975
KTG5672 <zzang5992@gmail.com>
2025-08-04 23:37:28 +0900

    refactor: comment Calculator.plus

--- unit-test-example/src/main/java/calculator/Calculator.java
+++ unit-test-example/src/main/java/calculator/Calculator.java
@@ -1,7 +1,16 @@
 package calculator;
 
+/**
+ * 계산 정적 메서드를 제공하는 클래스
+ */
 public class Calculator {
 
+    /**
+     * 두 수를 더하여 반환해주는 정적 메서드
+     * @param x 더할 수
+     * @param y 더할 수
+     * @return int 결과
+     */
     public static int plus(int x, int y) {
         return x + y;
     }
