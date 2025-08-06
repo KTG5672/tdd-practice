bf4717dffdd24e7ef984b7b2c4e1c4a4a53bebf4
KTG5672 <zzang5992@gmail.com>
2025-08-06 22:43:34 +0900

    feat: implement PasswordStrengthMeter.meter with short password returning NORMAL

--- unit-test-example/src/main/java/password/PasswordStrengthMeter.java
+++ unit-test-example/src/main/java/password/PasswordStrengthMeter.java
@@ -3,6 +3,9 @@ package password;
 public class PasswordStrengthMeter {
 
     public PasswordLevel meter(String password) {
+        if (password.length() < 8) {
+            return PasswordLevel.NORMAL;
+        }
         return PasswordLevel.STRONG;
     }
 }
