3507391048cdcd325a68aefc549b1806c4de1a1d
KTG5672 <zzang5992@gmail.com>
2025-08-06 22:50:09 +0900

    feat: implement PasswordStrengthMeter.meter with missing number returning NORMAL

--- unit-test-example/src/main/java/password/PasswordStrengthMeter.java
+++ unit-test-example/src/main/java/password/PasswordStrengthMeter.java
@@ -6,6 +6,18 @@ public class PasswordStrengthMeter {
         if (password.length() < 8) {
             return PasswordLevel.NORMAL;
         }
+
+        char[] charArray = password.toCharArray();
+        boolean containNumber = false;
+        for (char c : charArray) {
+            if (c >= '0' && c <= '9') {
+                containNumber = true;
+                break;
+            }
+        }
+        if (!containNumber) {
+            return PasswordLevel.NORMAL;
+        }
         return PasswordLevel.STRONG;
     }
 }
