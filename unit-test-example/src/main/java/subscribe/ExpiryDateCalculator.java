6351ccc4e035c47fe47d4f486088f7c9da81ea92
KTG5672 <zzang5992@gmail.com>
2025-08-13 23:22:42 +0900

    test: add failing test for ExpiryDateCalculator.calculate - 만원 결제 시 납부일 기준 한달 뒤 날짜를 리턴

---
+++ unit-test-example/src/main/java/subscribe/ExpiryDateCalculator.java
@@ -0,0 +1,11 @@
+package subscribe;
+
+import java.time.LocalDate;
+
+public class ExpiryDateCalculator {
+
+    public LocalDate calculate(LocalDate billingDate, int payAmount) {
+        return null;
+    }
+
+}
---
+++ unit-test-example/src/test/java/subscribe/ExpiryDateCalculatorTest.java
@@ -0,0 +1,27 @@
+package subscribe;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 1. 서비스를 사용하려면 매달 1만 원을 선불로 납부한다. 납부일 기준으로 한 달 뒤가 서비스 만료일이 된다.
+ * 2. 2개월 이상 요금을 납부할 수 있다.
+ * 3. 10만 원을 납부하면 서비스를 1년 제공한다
+ */
+public class ExpiryDateCalculatorTest {
+
+    @Test
+    void 만원을_납부하면_한달_뒤가_만료일이_된다() {
+        LocalDate billingDate = LocalDate.of(2025, 8, 13);
+        int payAmount = 10_000;
+
+        ExpiryDateCalculator expiryDateCalculator = new ExpiryDateCalculator();
+        LocalDate expiryDate = expiryDateCalculator.calculate(billingDate, payAmount);
+
+        assertThat(expiryDate).isEqualTo(billingDate.plusDays(1));
+    }
+
+
+}
