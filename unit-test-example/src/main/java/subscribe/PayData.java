fe84ca8404e2baae8133493ef45f4056a690d81c
KTG5672 <zzang5992@gmail.com>
2025-08-15 18:51:34 +0900

    refactor: ExpiryDateCalculator.calculate parameters extract PayData class

---
+++ unit-test-example/src/main/java/subscribe/PayData.java
@@ -0,0 +1,19 @@
+package subscribe;
+
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@Builder
+public class PayData {
+
+    private LocalDate billingDate;
+    private int payAmount;
+
+}
