e30ca7c79c23d2db6102c09e6ad44acd55519a1f
KTG5672 <zzang5992@gmail.com>
2025-08-15 19:24:52 +0900

    feat: implement ExpiryDateCalculator.calculate - 첫 납부일과 만료일 일자가 다를 때 만원 납부

--- unit-test-example/src/main/java/subscribe/ExpiryDateCalculator.java
+++ unit-test-example/src/main/java/subscribe/ExpiryDateCalculator.java
@@ -1,10 +1,18 @@
 package subscribe;
 
+import static java.util.Objects.nonNull;
+
 import java.time.LocalDate;
 
 public class ExpiryDateCalculator {
 
     public LocalDate calculate(PayData payData) {
+        if (nonNull(payData.getFirstBillingDate())) {
+            LocalDate candidateExp = payData.getBillingDate().plusMonths(1);
+            if (candidateExp.getDayOfMonth() != payData.getFirstBillingDate().getDayOfMonth()) {
+                return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
+            }
+        }
         return payData.getBillingDate().plusMonths(1);
     }
 
