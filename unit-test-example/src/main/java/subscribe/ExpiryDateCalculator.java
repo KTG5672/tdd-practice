998cf36ec22af7ad2c1f173ef335286156b8d3ff
KTG5672 <zzang5992@gmail.com>
2025-08-16 20:10:24 +0900

    refactor: ExpiryDateCalculator.calculate extract adjustByFirstBillingDate

--- unit-test-example/src/main/java/subscribe/ExpiryDateCalculator.java
+++ unit-test-example/src/main/java/subscribe/ExpiryDateCalculator.java
@@ -11,19 +11,24 @@ public class ExpiryDateCalculator {
         LocalDate billingDate = payData.getBillingDate();
         LocalDate firstBillingDate = payData.getFirstBillingDate();
         int monthsToAdd = payData.getPayAmount() / 10_000;
+        LocalDate candidateExp = billingDate.plusMonths(monthsToAdd);
         if (nonNull(firstBillingDate)) {
-            LocalDate candidateExp = billingDate.plusMonths(monthsToAdd);
-            YearMonth candidateExpYearMonth = YearMonth.from(candidateExp);
-            if (candidateExpYearMonth.lengthOfMonth() < firstBillingDate.getDayOfMonth()) {
-                return candidateExp.withDayOfMonth(
-                    candidateExpYearMonth.lengthOfMonth()
-                );
-            }
-            if (candidateExp.getDayOfMonth() != firstBillingDate.getDayOfMonth()) {
-                return candidateExp.withDayOfMonth(firstBillingDate.getDayOfMonth());
-            }
+            candidateExp = adjustByFirstBillingDate(candidateExp, firstBillingDate);
         }
-        return billingDate.plusMonths(monthsToAdd);
+        return candidateExp;
+    }
+
+    private LocalDate adjustByFirstBillingDate(LocalDate candidateExp, LocalDate firstBillingDate) {
+        YearMonth candidateExpYearMonth = YearMonth.from(candidateExp);
+        if (candidateExpYearMonth.lengthOfMonth() < firstBillingDate.getDayOfMonth()) {
+            return candidateExp.withDayOfMonth(
+                candidateExpYearMonth.lengthOfMonth()
+            );
+        }
+        if (candidateExp.getDayOfMonth() != firstBillingDate.getDayOfMonth()) {
+            return candidateExp.withDayOfMonth(firstBillingDate.getDayOfMonth());
+        }
+        return candidateExp;
     }
 
 }
