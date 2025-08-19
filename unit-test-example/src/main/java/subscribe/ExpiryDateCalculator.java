7756c38b3c8c8af87b3118ba86c04563a1e08632
KTG5672 <zzang5992@gmail.com>
2025-08-19 23:11:51 +0900

    refactor: ExpiryDateCalculator.calculate extract calculateMonthsToAdd method and constants

--- unit-test-example/src/main/java/subscribe/ExpiryDateCalculator.java
+++ unit-test-example/src/main/java/subscribe/ExpiryDateCalculator.java
@@ -7,16 +7,27 @@ import java.time.YearMonth;
 
 public class ExpiryDateCalculator {
 
+    private static final int YEARS_UNIT_AMOUNT = 100_000;
+    private static final int MONTHS_UNIT_AMOUNT = 10_000;
+
     public LocalDate calculate(PayData payData) {
         LocalDate billingDate = payData.getBillingDate();
         LocalDate firstBillingDate = payData.getFirstBillingDate();
-        int yearsToAdd = payData.getPayAmount() / 100_000;
-        int monthsToAdd = ((payData.getPayAmount() % 100_000) / 10_000) + (yearsToAdd * 12);
+        int payAmount = payData.getPayAmount();
+
+        int monthsToAdd = calculateMonthsToAdd(payAmount);
+
         LocalDate candidateExp = billingDate.plusMonths(monthsToAdd);
         if (nonNull(firstBillingDate)) {
             candidateExp = adjustByFirstBillingDate(candidateExp, firstBillingDate);
         }
         return candidateExp;
+    }
+
+    private int calculateMonthsToAdd(int payAmount) {
+        int yearsToAdd = payAmount / YEARS_UNIT_AMOUNT;
+        int monthsToAdd = (payAmount % YEARS_UNIT_AMOUNT) / MONTHS_UNIT_AMOUNT;
+        return (yearsToAdd * 12) + monthsToAdd;
     }
 
     private LocalDate adjustByFirstBillingDate(LocalDate candidateExp, LocalDate firstBillingDate) {
