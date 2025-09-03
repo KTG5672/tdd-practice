47bc007640deb54e2755075ae7362bd37e1e5886
KTG5672 <zzang5992@gmail.com>
2025-09-03 23:11:46 +0900

    test: add failing test for UserRegister.register - 이미 같은 ID가 존재하면 가입 실패

--- unit-test-example/src/test/java/user/UserRegisterTest.java
+++ unit-test-example/src/test/java/user/UserRegisterTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 /**
  * 회원가입 서비스 테스트
  * - 암호가 약하면 가입이 불가
+ * - 동일 ID 가 있을 시 가입이 불가
  */
 public class UserRegisterTest {
 
@@ -28,6 +29,13 @@ public class UserRegisterTest {
         weakPasswordChecker.setWeak(true);
         assertThatThrownBy(() -> userRegister.register("id", "pass", "email"))
             .isInstanceOf(WeakPasswordException.class);
+    }
+
+    @Test
+    @DisplayName("이미 같은 ID가 존재하면 가입 실패")
+    void dupIdExists() {
+        assertThatThrownBy(() -> userRegister.register("id", "pass", "email"))
+            .isInstanceOf(DupIdException.class);
     }
 
 }
